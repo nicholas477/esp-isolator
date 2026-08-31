cb3209efb5f1359b6c34693cfa92883430d24b99
EPICGameGuy <nicholas2.chalkley@gmail.com>
2026-08-30 22:29:54 -0400

    Added an output arg, added help stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clap::Parser;
 use egg_esp_lib::record::parse_records;
@@ -6,10 +6,14 @@ use std::collections::hash_set::HashSet;
 use zip::{CompressionMethod, write::FileOptions};
 
 #[derive(Parser, Debug)]
-#[command(version, about = "A simple CLI tool example", long_about = None)]
+#[command(version, about = "Grabs the statics from an ESP file, then packages the meshes, textures, and the ESP file into a single zip.", long_about = None)]
 struct Args {
-    /// ESP file to isolate resources from
-    file: String,
+    /// ESP file to isolate meshes and textures from
+    file: PathBuf,
+
+    /// (Optional) Output file path. If not specified, the zip file will be created in the same directory as the input ESP file.
+    #[arg(short, long)]
+    output: Option<PathBuf>,
 }
 
 fn add_file(files: &mut HashSet<String>, file_path: &str) -> bool {
@@ -30,7 +34,10 @@ fn collect_files_from_record(
 
         let nif_file = plugin_path.join(Path::new("meshes")).join(&stat.model.path);
 
-        add_file(files, Path::new("meshes").join(&stat.model.path).to_str().unwrap());
+        add_file(
+            files,
+            Path::new("meshes").join(&stat.model.path).to_str().unwrap(),
+        );
 
         niflib::get_nif_texture_filepaths(nif_file.to_str().unwrap())
             .iter()
@@ -67,41 +74,69 @@ fn zip_files(
             zip.start_file(file_path, options)?;
             let mut f = std::fs::File::open(path)?;
             std::io::copy(&mut f, &mut zip)?;
+        } else {
+            eprintln!("Warning: File not found: {}", path.display());
+            return Err(format!("File not found: {}", path.display()).into());
         }
     }
 
+    // Redundant since the zip is finished when the ZipWriter is dropped
     zip.finish()?;
+
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    let plugin_path = Path::new(&args.file).parent().unwrap();
+    if !args.file.is_file() {
+        eprintln!(
+            "Error: The provided path is not a file: \"{}\"",
+            args.file.display()
+        );
+        std::process::exit(1);
+    }
+
+    let zip_path = args
+        .output
+        .unwrap_or(Path::new(&args.file).with_extension("zip"));
+
+    let plugin_path = args.file.parent().unwrap();
     let plugin_data = std::fs::read(&args.file)?;
 
-    let records = parse_records(&plugin_data)?;
+    let records = parse_records(&plugin_data)
+        .inspect_err(|e| eprintln!("Failed to parse records from file: {}", e))?;
 
+    // Insert the ESP file itself into the set of files to be zipped
     let mut files = HashSet::new();
-    files.insert(Path::new(&args.file).file_name().unwrap().to_str().unwrap().to_string());
+    files.insert(
+        Path::new(&args.file)
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_string(),
+    );
 
     collect_files_from_records(&records, plugin_path, &mut files);
 
-    println!("-- Found {} unique files:", files.len());
-    for file in &files {
-        println!("Found file: {}", file);
+    {
+        println!("-- Zipping {} files:", files.len());
+        let mut files = files.iter().collect::<Vec<&String>>();
+        files.sort();
+        for file in &files {
+            println!("\t-- {}", file);
+        }
     }
 
-    let zip_path = Path::new(&args.file).with_extension("zip");
-    println!("Creating zip file at: {:?}", zip_path);
+    println!("Creating zip file at: \"{}\"", zip_path.display());
 
-    zip_files(
-        &files,
-        plugin_path,
-        &zip_path,
-    )?;
+    zip_files(&files, plugin_path, &zip_path)?;
 
-    println!("Zip file created successfully at: {:?}", zip_path);
+    println!(
+        "Zip file created successfully at: \"{}\"",
+        zip_path.display()
+    );
 
     Ok(())
 }
